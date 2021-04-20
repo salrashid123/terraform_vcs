e977fd8f0b47a215d0feb3939a60ca5f8a6cc215
salmaan rashid <salrashid123@gmail.com>
2021-04-20 15:41:23 -0400

    add

--- hello_world/default.tf
+++ hello_world/default.tf
@@ -2,13 +2,13 @@
 terraform {
   required_providers {
     http = {
-      source = "hashicorp/http"
       version = "2.1.0"
     }
   }
 }
 
 provider "http" {
+  # Configuration options
 }
 
 variable "user_agent" {
