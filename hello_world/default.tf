04d321fbf2733eb3877608996b41e98ad759c80f
salmaan rashid <salrashid123@gmail.com>
2021-04-20 17:04:30 -0400

    update

--- hello_world/default.tf
+++ hello_world/default.tf
@@ -1,7 +1,7 @@
-
 terraform {
   required_providers {
     http = {
+      source = "hashicorp/http"
       version = "2.1.0"
     }
   }
--- main.tf
+++ main.tf
@@ -1,10 +1,4 @@
 terraform {
-  required_providers {
-    http = {
-      source = "hashicorp/http"
-      version = "2.1.0"
-    }
-  }
   required_version = "~> 0.12"
   backend "remote" {
     hostname      = "app.terraform.io"
